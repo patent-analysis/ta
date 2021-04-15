67ffa88beb1495b06270576a760a09c96416b76e
Parvinderjit Singh <parv112281@hotmail.com>
2021-04-15 11:53:52 -0400

    converted to using fitz and ocr for extracting patent id, added Anna's code for extracting epitopes and sequence information

--- src/process_documents/app.py
+++ src/process_documents/app.py
@@ -4,20 +4,22 @@ import urllib
 import os
 import logging
 import textract
-import PyPDF2
 import re
-import io
 import requests
 import json
+import fitz
 from patent import Patent
+from seqlisting import SeqListing
 from collections import Counter
 
-LOCAL_STACK_URL = 'http://host.docker.internal:4566' # mac specific setting, windows look elsewhere
+LOCAL_STACK_URL = 'http://host.docker.internal:4566' # mac specific setting, windows should use localhost
 DOC_NUMBER_REGEX = '((US|us)\s?([,|\/|\s|\d|&])+\s?([a-zA-Z]\d))'
 PAT_ID_PATTERN = "US\s?\d*[A-B][0-9]"
-BASE_URL = 'https://uspto-documents-storage.s3.amazonaws.com/docs/'
+PATENT_BASE_URL = 'https://uspto-documents-storage.s3.amazonaws.com/docs/'
+LISTINGS_BASE_URL = 'https://uspto-documents-storage.s3.amazonaws.com/seq/'
 FULL_PDF_PATH = 'full_pdf_temp.pdf'
 FIRST_PAGE_PATH = 'page_one_pdf_temp.pdf'
+TMP_IMAGE_PATH = 'img_temp.png'
 
 doc_num_matcher = re.compile(DOC_NUMBER_REGEX)
 logger = logging.getLogger()
@@ -31,11 +33,26 @@ else:
     s3_client = boto3.client('s3')
 
 
+def fetch_seq_listing(patent_id):
+    listing_path = patent_id + '.xml'
+    logger.info("requesting " + LISTINGS_BASE_URL + listing_path)
+    response = requests.get(LISTINGS_BASE_URL + listing_path)
+    logger.info("remote request status: " + str(response.status_code))
+
+    if response.status_code == 200:
+        with open(listing_path, 'wb') as f:
+            f.write(response.content)
+        return SeqListing(patent_id)
+    else:
+        return None
+
+
 def fetch_patent_data(patent_id):
     patent_path = patent_id + '.xml'
-    logger.info("requesting " + BASE_URL + patent_path)
-    response = requests.get(BASE_URL + patent_path)
+    logger.info("requesting " + PATENT_BASE_URL + patent_path)
+    response = requests.get(PATENT_BASE_URL + patent_path)
     logger.info("remote request status: " + str(response.status_code))
+
     if response.status_code == 200:
         with open(patent_path, 'wb') as f:
             f.write(response.content)
@@ -44,30 +61,15 @@ def fetch_patent_data(patent_id):
         return None    
 
 
-def extract_first_page():
-    reader = PyPDF2.PdfFileReader(FULL_PDF_PATH)
-    # extract first page and save
-    page = reader.getPage(0)
-    page_writer = PyPDF2.PdfFileWriter()
-    page_writer.addPage(page)
-    with open(FIRST_PAGE_PATH, "wb") as writer_stream:
-        page_writer.write(writer_stream)
-
-
 def extract_patent_id():
-    parsed_text = textract.process(FULL_PDF_PATH).decode('utf-8')
-    if parsed_text.startswith('\x0c\x0c\x0c'):
-        extract_first_page()
-        parsed_text = textract.process(FIRST_PAGE_PATH, method='tesseract').decode('utf-8')
-
-    pat_num_matches = doc_num_matcher.findall(parsed_text)
-    pat_num_count = Counter(pat_num_matches)
-    raw_pat_id = pat_num_count.most_common(1)[0]
-
-    while (isinstance(raw_pat_id, tuple)):
-        raw_pat_id = raw_pat_id[0]
+    doc = fitz.open(FULL_PDF_PATH)
+    page = doc.loadPage(0)
+    pix = page.getPixmap(matrix=fitz.Matrix(5, 5))
+    pix.writePNG(TMP_IMAGE_PATH)
+    parsed_text = textract.process(TMP_IMAGE_PATH, method='tesseract').decode('utf-8')
 
     # extract patent id
+    raw_pat_id = re.search(DOC_NUMBER_REGEX, parsed_text).group(0)
     patent_id = re.sub('[,|&|\s|/]', '',raw_pat_id).strip('0')
     return patent_id
 
@@ -83,8 +85,11 @@ def parse_doc_text(bucket, key):
 
     # extract patent id from first page
     patent_id = extract_patent_id()
-    
-    return fetch_patent_data(patent_id)
+
+    # extract and return patent metadata and sequence listing
+    patent = fetch_patent_data(patent_id)
+    seq_listing = fetch_seq_listing(patent_id)
+    return patent, seq_listing
 
 
 def lambda_handler(event, context):
@@ -98,16 +103,24 @@ def lambda_handler(event, context):
         event['Records'][0]['s3']['object']['key'])
 
     try:
-        response = parse_doc_text(bucket, object_key)
+        patent, seq_listing = parse_doc_text(bucket, object_key)
         logger.info("Done handling event")
-        if response == None:
-            logger.error('unable to fetch patent data')
-            return 'Failure'
+        result = 'Success'
+
+        if patent and seq_listing:
+            logger.info("Patent Name: " + patent.patentName)
+            logger.info("SeqListing count: " + seq_listing.seqCount)
+            return result
         else:
-            for field in response.__dict__:
-                if field != 'description':
-                    logger.info(field + ':' + str(response.__dict__[field]))
-            return 'Success'
+            result = ''
+
+        if patent == None:
+            logger.error('unable to fetch patent data')
+            result += 'Patent Failure'
+        if seq_listing == None:
+            logger.error('unable to fetch patent data')
+            result += ' SeqListing Failure'
+        return result.lstrip()
 
     except Exception as e:
         logger.error("Error processing key {} Event {} Error: {}".format(
