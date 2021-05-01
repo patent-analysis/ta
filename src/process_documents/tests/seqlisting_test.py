368b0cde80b8339c47487a3204be0a9c5d247b5e
kaliweh <khalidaliweh@gmail.com>
2021-05-01 00:02:47 -0400

    parsing the new seq listing file format

--- src/process_documents/tests/seqlisting_test.py
+++ src/process_documents/tests/seqlisting_test.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 from classes.seqlisting import SeqListing
-mock_xml_document1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mocks', 'seq_US8829165B2.xml')
+mock_xml_document1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mocks', 'seq_US10787484B2.xml')
 
 def test_xml_parsing_1():
     seq_listing = SeqListing(mock_xml_document1,'US8828405B2')
