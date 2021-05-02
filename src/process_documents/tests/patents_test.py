61b06bfb565c88674cedf4399d113540e9c3d95d
kaliweh <khalidaliweh@gmail.com>
2021-05-01 23:48:03 -0400

    edit regex patterns

--- src/process_documents/tests/patents_test.py
+++ src/process_documents/tests/patents_test.py
@@ -3,6 +3,7 @@ import os
 from classes.patent import Patent
 mock_xml_document1 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mocks', '8828405.xml')
 mock_xml_document2 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mocks', 'US10787484B2.xml')
+mock_xml_document3 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mocks', 'US8829165B2.xml')
 
 def test_xml_parsing_1():
     patent = Patent(mock_xml_document1,'US8828405B2')
@@ -31,3 +32,8 @@ def test_claimed_residues():
     patent = Patent(mock_xml_document2,'US10787484')
     # TODO: update with expected residues...
     assert patent.mentionedResidues != []
+
+def test_claimed_residues_3():
+    patent = Patent(mock_xml_document3,'US8829165B2')
+    # TODO: update with expected residues...
+    assert patent.mentionedResidues != []    
